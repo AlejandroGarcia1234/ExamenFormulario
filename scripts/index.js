d131557a91416a4627b9f7582bba1e73278d87e9
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2024-01-17 09:45:05 +0100

    Script completado, de mommento

--- scripts/index.js
+++ scripts/index.js
@@ -18,14 +18,19 @@ const validar = evento => {
     !/^[a-zA-Z0-9]*$/.test(nombre.value.trim()) && mensajeErrores.push('El nombre no tiene caracteres válidos')
     !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(correo.value.trim()) && mensajeErrores.push('Introduce una dirección de correo electrónico válida')
     mensaje.value.trim().length < 10 && mensajeErrores.push('Mensaje demasiado corto')
+    !/^[1]?[0-9]{1,2}$/.test(edad.value.trim()) && mensajeErrores.push('El campo edad es obligatorio')
 
-!/^[1]?[0-9]{1,2}$/.test(edad.value.trim()) && mensajeErrores.push('El campo edad es obligatorio')
-    if(mensajeErrores > 0 && confirm('¿Estas seguro/a de enviar el formulario?')){
+    if(mensajeErrores.length === 0 && confirm('¿Estás seguro/a de enviar el formulario?')) {
         formulario.submit()
-    }else if(mensajeErrores > 0)
-
-
-
+    }else if(mensajeErrores.length > 0) {
+        errores.textContent = ""
+        console.log(mensajeErrores)
+        mensajeErrores.forEach(function (mensaje) {
+            const miLi = document.createElement('li')
+            miLi.textContent = mensaje
+            errores.appendChild(miLi)
+        })
+    }
 }
 
 formulario.addEventListener('submit', validar)
